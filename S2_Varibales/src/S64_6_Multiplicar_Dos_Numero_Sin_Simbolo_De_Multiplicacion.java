74fe85a8054137e07f91b27443659f44d947cc53
EDS <n3.edson@hotmail.com>
2024-06-06 13:11:14 -0500

    Revisar ejercicio 64.6 y debbugear

--- S2_Varibales/src/S64_6_Multiplicar_Dos_Numero_Sin_Simbolo_De_Multiplicacion.java
+++ S2_Varibales/src/S64_6_Multiplicar_Dos_Numero_Sin_Simbolo_De_Multiplicacion.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class S64_6_Multiplicar_Dos_Numero_Sin_Simbolo_De_Multiplicacion {
+import java.util.Scanner;
+
+public class S64_6_Multiplicar_Dos_Numero_Sin_Simbolo_De_Multiplicacion {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero: ");
+        double numero = sc.nextDouble();
+
+        System.out.println("Ingrese el segundo numero: ");
+        double numero2 = sc.nextDouble();
+
+        boolean numeroNegativo = (numero < 0 && numero2 > 0) || (numero > 0 && numero2 <0);
+
+        numero = Math.abs(numero);
+        numero2 = Math.abs(numero2);
+
+        int resultado = 0;
+
+        for (int i = 0; i < numero2; i++) {
+            resultado += numero;
+        }
+
+
+        if(numeroNegativo){
+            resultado = -resultado;
+        }
+
+        System.out.println("El resultado de la multiplicación es: " + resultado);
+
+
+
+    }
 }
